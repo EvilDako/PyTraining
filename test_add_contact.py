1e6deabfb870e561a074e24b43d346815a586deb
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-09-20 22:02:44 +0300

    Lesson1_Task3 Создание теста на добавление нового контакта

---
+++ test_add_contact.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+from contact import Contact
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def contact_creation(self, wd, contact):
+        # init creation new contact
+        wd.find_element_by_link_text("add new").click()
+        # fill new contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.tel_home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.tel_mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.tel_work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.tel_fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        # здесь непонятно как параметризировать выбор значений из выпадающего списка
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[1]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[1]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[1]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[19]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[19]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[11]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[11]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        # год не указываю как параметр, т.к. непонятно как обработать выбор дня и месяца
+        wd.find_element_by_name("byear").send_keys("1987")
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[15]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[15]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[12]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[12]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        # год не указываю как параметр, т.к. непонятно как обработать выбор дня и месяца
+        wd.find_element_by_name("ayear").send_keys("2000")
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.phone2)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+        # submit creation new contact
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.contact_creation(wd, Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="VANO", title="Sir", company="SUPERCOMPANY", address="Moscow, Old Arbat, 9",
+                              tel_home="495555555", tel_mobile="89009009090", tel_work="495123456", tel_fax="+899999999", email="ivanov@mail.ru", email2="ivan@mail.ru", email3="ivanovich@mail.ru", homepage="www.vanyusha.com",
+                              address2="none", phone2="none", notes="many funny comments"))
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
