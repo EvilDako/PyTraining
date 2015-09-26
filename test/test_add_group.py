fe4941ff530299d2ff7efb6c01f7e0d4db2f3e80
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-09-27 01:40:07 +0300

    Lesson2_Task5: Реализована двухуровневая архитектура тестов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group_creation(Group(name="GROUP_N", header="Header of the GROUP_N", footer="Footer of the GROUP_N"))
+    app.group.create(Group(name="GROUP_N", header="Header of the GROUP_N", footer="Footer of the GROUP_N"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group_creation(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
