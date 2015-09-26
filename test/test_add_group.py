f366e11d76115b1bb6249f688dcad13a6ea67fc1
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-09-27 01:13:07 +0300

    Выделен вспомогательный класс SessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.group_creation(Group(name="GROUP_N", header="Header of the GROUP_N", footer="Footer of the GROUP_N"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.group_creation(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
