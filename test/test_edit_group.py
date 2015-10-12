8578ddaec9a8a721dbc62412b7b2fcbfe627a4a6
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-10-12 17:55:30 +0300

    Релизованы проверки для тестов на группы. ПОлучение и сортировка списков групп.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,6 +7,10 @@ def test_edit_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="name_name", header="head_head"))
     old_groups = app.group.get_group_list()
-    app.group.edit(Group(name="new_name", header="new_header", footer="new_footer"))
+    group = Group(name="new_name", header="new_header", footer="new_footer")
+    group.id = old_groups[0].id
+    app.group.edit(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
