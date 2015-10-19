b0c7fbf5c5d52ce62efa16f6b793926132c15c3c
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-10-20 00:55:10 +0300

    Lesson5_Task15: Выполнена параметризация тестов для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
+testdata = [
+    Contact(firstname=firstname, middlename=middlename, lastname=lastname, nickname=nickname, title=title, company=company, address=address, tel_home=tel_home, email=email)
+    for firstname in ["", random_string("firstname", 15)]
+    for middlename in ["", random_string("middlename", 15)]
+    for lastname in ["", random_string("lastname", 15)]
+    for nickname in ["", random_string("nickname", 15)]
+    for title in ["", random_string("title", 15)]
+    for company in ["", random_string("company", 15)]
+    for address in ["", random_string("address", 30)]
+    for tel_home in ["", random_string("tel_home", 15)]
+    for email in ["", random_string("email", 15)]
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="VANO", title="Sir", company="SUPERCOMPANY", address="Moscow, Old Arbat, 9",
-                          tel_home="495555555", tel_mobile="89009009090", tel_work="495123456", tel_fax="+899999999", email="ivanov@mail.ru", email2="ivan@mail.ru", email3="ivanovich@mail.ru", homepage="www.vanyusha.com",
-                          address2="none", phone2="none", notes="many funny comments")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == app.contact.count()
