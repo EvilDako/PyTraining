4451ac0f7b4ead3bb040f6229f09e72b48b1a383
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-10-26 00:25:18 +0300

    выделены отдельно тестовые данные

---
+++ data/groups.py
@@ -0,0 +1,17 @@
+__author__ = 'dako'
+# -*- coding: utf-8 -*-
+from model.group import Group
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 20)]
+    ]
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [
-    Contact(firstname=firstname, middlename=middlename, lastname=lastname, nickname=nickname, title=title, company=company, address=address, tel_home=tel_home, email=email)
-    for firstname in ["", random_string("firstname", 15)]
-    for middlename in ["", random_string("middlename", 15)]
-    for lastname in ["", random_string("lastname", 15)]
-    for nickname in ["", random_string("nickname", 15)]
-    for title in ["", random_string("title", 15)]
-    for company in ["", random_string("company", 15)]
-    for address in ["", random_string("address", 30)]
-    for tel_home in ["", random_string("tel_home", 15)]
-    for email in ["", random_string("email", 15)]
-]
+from data.contacts import testdata
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
