75d3506bc9ad0b14361f87a0d5dd0a0f9837b3c1
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-11-03 16:50:20 +0300

    добавлен тест на сравнение списков групп, полученных через интерфейси и через запросы к БД

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 __author__ = 'dako'
 from mysql import connector
+from model.group import Group
 
 class DbFixture():
     def __init__(self, host, port, database, user, password):
@@ -9,6 +10,18 @@ class DbFixture():
         self.user = user
         self.password = password
         self.connection = connector.connect(host=host, port=port, database=database, user=user, password=password)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
 
     def destroy(self):
         self.connection.close()
