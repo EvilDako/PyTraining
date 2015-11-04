ad597e62b0fc40f7d46182a5ebdc4261bb3c8f34
Dmitry Lebedev <d.l.lebedev@mail.ru>
2015-11-04 18:20:06 +0300

    Lesson7_Task20: Переделаны тесты на сравенение данных, загруженных из БД

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,7 +3,7 @@ __author__ = 'dako'
 from model.contact import Contact
 import random
 
-def test_edit_contact(app, db):
+def test_edit_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="test"))
     old_contacts = db.get_contact_list()
@@ -17,3 +17,5 @@ def test_edit_contact(app, db):
     assert len(old_contacts) == app.contact.count()
     #old_contacts[index] = contact
     #assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,7 +3,7 @@ __author__ = 'dako'
 from model.group import Group
 import random
 
-def test_edit_group(app, db):
+def test_edit_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="name_name", header="head_head"))
     old_groups = db.get_group_list()
@@ -12,7 +12,6 @@ def test_edit_group(app, db):
     group_new.id = group.id
     app.group.edit_group_by_id(group.id, group_new)
     new_groups = db.get_group_list()
-    assert len(old_groups) == app.group.count()
-    #Проверка не отрабатывает :(
-    #old_groups.append(group_new)
     #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
